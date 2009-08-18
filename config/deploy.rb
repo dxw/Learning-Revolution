e062558de44b3bcd0be4abb884cf0fc814f85992
James Darling <james@abscond.org>
2009-08-18 19:13:07 +0100

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+set :stages, %w(staging production)
+set :default_stage, 'staging'
+require 'capistrano/ext/multistage'
+
+set :application, "learning_revolution"
+
+default_run_options[:pty] = true
+set :repository,  "git@github.com:dxw/Learning-Revolution.git"
+set :scm, "git"
+set :branch, "deploy"
+set :git_shallow_clone, 1
+
+
+set :symlinked_dirs, [
+  # ['index', 'search/index']
+]
+
+namespace :deploy do
+  
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+     
+  desc "Creates additional symlinks and restarts sphinx"
+  task :after_update_code do
+    if symlinked_dirs
+      symlinked_dirs.each do |item| 
+        src, dest = item
+        run "ln -nfs #{storage_path}/#{dest} #{current_path}/#{src}"
+      end
+    end
+    run "ln -fs #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
+  end
+  
+end
