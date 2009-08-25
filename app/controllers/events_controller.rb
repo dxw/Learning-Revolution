e201666a9643ee5917525516e6f5336b2f6778d7
James Darling <james@abscond.org>
2009-08-25 17:45:15 +0100

    quick fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   def index
+    params[:filter] ||= {}
     @first_day_of_month = Time.parse("#{params[:month]} #{params[:year]}")
     @events = Event.find_for_month_with_filter(@first_day_of_month, params[:filter])
     @event_counts = Event.counts_for_month(@first_day_of_month)
