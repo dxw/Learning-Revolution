97a5453f5fcebe679712c64738956e9e07f97fa0
James Darling <james@abscond.org>
2009-08-28 16:24:53 +0100

    fix event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
+  before_filter :ensure_filters
+  
   def index
-    params[:filter] ||= {}
     @first_day_of_month = Time.parse("#{params[:month]} #{params[:year]}")
     @events = Event.find_by_month_with_filter_from_params(@first_day_of_month, params[:filter])
     @event_counts = Event.counts_for_month(@first_day_of_month)
@@ -15,6 +16,10 @@ class EventsController < ApplicationController
   
   def fix_path
     redirect_to path_for_event(@event) unless request.path == path_for_event(@event)
+  end
+  
+  def ensure_filters
+    params[:filter] ||= {}
   end
 
 end
