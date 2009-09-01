cb928014db403e16ad1ba333be75d96f400c3ad3
James Darling <james@abscond.org>
2009-09-01 14:15:32 +0100

    added location zooming to map

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,7 @@ class EventsController < ApplicationController
   def index
     @first_day_of_month = Time.parse("#{params[:month]} #{params[:year]}")
     if params[:view] == "map"
+      set_map_bounds
       @venues = Venue.find_venues_by_event_params(params[:filter])
     else
       @events = Event.find_by_month_with_filter_from_params(@first_day_of_month, params[:filter])
@@ -27,6 +28,20 @@ class EventsController < ApplicationController
   def ensure_filters
     params[:filter] ||= {}
     params[:view] ||= 'calendar'
+  end
+  
+  include Geokit::Geocoders
+  def set_map_bounds
+    if params[:filter][:location].blank?
+      @start_lat = 54
+      @start_lng = -4.064941
+      @start_zoom = 6
+    else
+      location = MultiGeocoder.geocode(params[:filter][:location] + " GB")
+      @start_lat = location.lat
+      @start_lng = location.lng
+      @start_zoom = 12
+    end
   end
 
 end
