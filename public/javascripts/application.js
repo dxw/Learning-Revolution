645006376f44c1f335af5c55a1f2aa65a3e1ef79
James Darling <james@abscond.org>
2009-09-02 17:43:27 +0100

    add correct number of vids and pics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@ Flickr = $.klass({
     var element = jQuery('<ul class="flickr_photos clearfix" id="flickr_photos_from_'+user_id+'"></ul>');
     this.element.replaceWith(element);
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id="+user_id+"&format=json&jsoncallback=?", function(data){ 
-      $.each(data.items.slice(0,6), function(i, item) { 
+      $.each(data.items.slice(0,8), function(i, item) { 
         element.append(
           '<li>'+
             '<a href="'+item.link+'">'+
@@ -20,7 +20,7 @@ YouTube = $.klass({
     var element = jQuery('<ul class="youtube_videos clearfix" id="youtube_videos_from_'+user_id+'"></ul>');
     this.element.replaceWith(element);
     $.getJSON("http://gdata.youtube.com/feeds/users/"+user_id+"/uploads?alt=json-in-script&callback=?", function(data){ 
-      $.each(data.feed.entry.slice(0,4), function(i, item) { 
+      $.each(data.feed.entry.slice(0,6), function(i, item) { 
         element.append(
           '<li>'+
             '<a href="'+item.link[0].href+'">'+
