9cec32f5db8533eb645b5d446279256a9cf0d98f
Tom Adams <tom@holizz.com>
2009-09-14 10:25:51 +0100

    Fix a typo, which was causing rake spec to except.

--- spec/helpers/application_help_spec.rb
+++ spec/helpers/application_help_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe ApplciationHelper do
+describe ApplicationHelper do
 
   #Delete this example and add some real ones or delete this file
   it "should be included in the object returned by #helper" do
