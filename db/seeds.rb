7bb8847af335acda7cf3f9b66c576147f149834f
Tom Adams <tom@holizz.com>
2009-09-22 22:11:12 +0100

    Hook up about and promote.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@ Page.create!(:slug => "about", :title => "About", :content => "<p>Blah blah blah
 Page.create!(:slug => "resources", :title => "Resources", :content => "<p>Sample content goes here.</p>")
 Page.create!(:slug => "terms-conditions", :title => "Terms and Conditions", :content => "<p>Lots of legalese.</p><p>You give us the right to publish your event. Duh.</p>")
 Page.create!(:slug => "privacy", :title => "Your Privacy", :content => "<p>Moar legalese.</p><p>we pwn all joo's data</p>")
+Page.create!(:slug => "promote-your-event", :title => "Promote Your Event", :content => "<p>Promote it. Promote!</p>")
