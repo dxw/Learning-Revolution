291d1a09b8e75ca4aedd37174d51293888ebc229
Tom Adams <tom@holizz.com>
2009-09-22 22:03:39 +0100

    Privacy policy.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 Page.create!(:slug => "about", :title => "About", :content => "<p>Blah blah blah.</p>")
 Page.create!(:slug => "resources", :title => "Resources", :content => "<p>Sample content goes here.</p>")
 Page.create!(:slug => "terms-conditions", :title => "Terms and Conditions", :content => "<p>Lots of legalese.</p><p>You give us the right to publish your event. Duh.</p>")
+Page.create!(:slug => "privacy", :title => "Your Privacy", :content => "<p>Moar legalese.</p><p>we pwn all joo's data</p>")
