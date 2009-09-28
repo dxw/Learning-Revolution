d1f8e83e5dc0fc7625fea8c03c9d916aab7af8ee
Harry Metcalfe <harry@thedextrousweb.com>
2009-09-28 12:54:20 +0100

    Added extra pages to seeds and made them conditional

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
-Page.create!(:slug => "about", :title => "About", :content => "<p>Blah blah blah.</p>")
-Page.create!(:slug => "resources", :title => "Resources", :content => "<p>Sample content goes here.</p>")
-Page.create!(:slug => "terms-conditions", :title => "Terms and Conditions", :content => "<p>Lots of legalese.</p><p>You give us the right to publish your event. Duh.</p>")
-Page.create!(:slug => "privacy", :title => "Your Privacy", :content => "<p>Moar legalese.</p><p>we pwn all joo's data</p>")
-Page.create!(:slug => "promote-your-event", :title => "Promote Your Event", :content => "<p>Promote it. Promote!</p>")
+Page.create!(:slug => "about", :title => "About", :content => "<p>About</p>") unless Page.find(:all, :conditions => {:slug => 'about'}).size > 0
+Page.create!(:slug => "terms-conditions", :title => "Terms and Conditions", :content => "<p>Terms</p>") unless Page.find(:all, :conditions => {:slug => 'terms-conditions'}).size > 0
+Page.create!(:slug => "privacy", :title => "Your Privacy", :content => "<p>Privacy</p>") unless Page.find(:all, :conditions => {:slug => 'privacy'}).size > 0
+Page.create!(:slug => "promote-your-event", :title => "Promote Your Event", :content => "<p>Promote</p>") unless Page.find(:all, :conditions => {:slug => 'promote-your-event'}).size > 0
+Page.create!(:slug => "frequently-asked-questions", :title => "Frequently Asked Questions", :content => "<p>FAQ</p>")  unless Page.find(:all, :conditions => {:slug => 'frequently-asked-questions'}).size > 0
+Page.create!(:slug => "copyright", :title => "Copyright", :content => "<p>Copyright</p>") unless Page.find(:all, :conditions => {:slug => 'copyright'}).size > 0
