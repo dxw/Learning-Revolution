f277aea2b8ea9c6d9e5fb5f972c3f61011eb05db
Harry Metcalfe <harry@thedextrousweb.com>
2009-09-28 16:13:42 +0100

    Added some flf data

--- app/helpers/admin/events_helper.rb
+++ app/helpers/admin/events_helper.rb
@@ -3,4 +3,5 @@ module Admin::EventsHelper
   def next_event(event)
     Event.find(:first, :conditions => ["(published IS NULL OR published != 1) AND id > ?", event.id], :order => "id ASC")
   end
+  
 end
