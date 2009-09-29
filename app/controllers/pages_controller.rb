20dd060977b509577a85190170d48c8a934d3e7e
Chris Adams <chris@stemcel.co.uk>
2009-09-29 21:33:04 +0100

    Added youtube video player to sidebar server side

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,9 @@ class PagesController < ApplicationController
 
   def show
     @page = Page.find(:first, :conditions => {:slug => params[:slug]})
+    @latest_youtube_vid_url = Hpricot(open('http://gdata.youtube.com/feeds/api/users/learnrevolution/uploads?orderby=updated'))
+    @latest_youtube_vid_url = (@latest_youtube_vid_url/'entry')[0].at('media:content').attributes['url']
     render_404 if @page.nil?
-
-
   end
 
   def news
@@ -16,5 +16,7 @@ class PagesController < ApplicationController
     # this needs to be given accessors to make it easier to iterate through
     @ningfeed = Hpricot(open('http://thelearningrevolution.ning.com/profiles/blog/feed?user=3eyqtja9xnzhb&xn_auth=no'))
     @ningfeed = (@ningfeed/'entry')[0..7]
+    @latest_youtube_vid_url = Hpricot(open('http://gdata.youtube.com/feeds/api/users/learnrevolution/uploads?orderby=updated'))
+    @latest_youtube_vid_url = (@latest_youtube_vid_url/'entry')[0].at('media:content').attributes['url']
   end
 end
