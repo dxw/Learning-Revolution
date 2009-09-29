281d6dff312dbe6eb2856910bfb62cdebae8298c
Tom Adams <tom@holizz.com>
2009-09-29 18:00:02 +0100

    Add host to errors.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,7 +4,7 @@ class Notifier < ActionMailer::Base
     recipients ['alerts@thedextrousweb.com']
     from       "the-server@learning.dev.thedextrousweb.com"
     subject    "Learning Rev: #{error_msg} #{exception.andand.class}"
-    body       :message => error_msg, :exception => exception, :referer => request.env['HTTP_REFERER'], :useragentstring => request.env['HTTP_USER_AGENT'], :remote_ip => request.env['REMOTE_ADDR'], :requesturi => request.env['REQUEST_URI']
+    body       :message => error_msg, :exception => exception, :referer => request.env['HTTP_REFERER'], :useragentstring => request.env['HTTP_USER_AGENT'], :remote_ip => request.env['REMOTE_ADDR'], :requesturi => request.env['REQUEST_URI'], :host => request.env['HTTP_HOST']
   end
 
 end
