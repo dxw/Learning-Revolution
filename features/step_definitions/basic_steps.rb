3ceb0307aadae140e09fa04a8398353262ffac88
Harry Metcalfe <harry@thedextrousweb.com>
2009-09-30 12:58:37 +0100

    Removed matcher

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,34 +1,3 @@
-require 'markup_validity' 
-
-module Spec
-  module Matchers
-    def be_atom
-      Matcher.new :be_atom do
-        match do |atom|
-          rng = Nokogiri::XML::RelaxNG(open('features/step_definitions/atom.rng').read)
-          feed = Nokogiri::XML(atom)
-          rng.valid?(feed)
-        end
-
-        failure_message_for_should do |actual|
-          atom
-        end
-      end
-    end
-    def be_icalendar
-      Matcher.new :be_icalendar do
-        match do |ical|
-          calendar = Icalendar.parse(ical)
-        end
-
-        failure_message_for_should do |actual|
-          calendar
-        end
-      end
-    end
-  end
-end
-
 Given "debugger" do
   debugger
 end
