47f80d6d3b9d4365cb4b87ebbed32c2b78692c43
Harry Metcalfe <harry@thedextrousweb.com>
2009-10-01 16:02:09 +0100

    Fixed dupe/unpub counts

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -29,7 +29,7 @@ class Admin::EventsController < Admin::AdminController
   end
   
   def count_unpublished
-    @count_unpublished ||= Event.find(:all, :conditions => {:published => false}).size
+    @count_unpublished ||= Event.find(:all, :conditions => 'published <> 1').size
   end
   
   def edit
--- app/helpers/admin/events_helper.rb
+++ app/helpers/admin/events_helper.rb
@@ -5,13 +5,13 @@ module Admin::EventsHelper
   end
   
   def get_unpublished_message(unpublished_count)
-    return "There are currently #{unpublished_count} unpublished events." if unpublished_count > 1
+    return "There are currently #{unpublished_count} unpublished events." if unpublished_count == 1
     
     "There is currently #{unpublished_count} unpublished event."
   end
   
   def get_duplicates_message(duplicates_count)
-    return "There are currently #{duplicates_count} possible duplicate events." if duplicates_count > 1
+    return "There are currently #{duplicates_count} possible duplicate events." if duplicates_count == 1
     
     "There is currently #{duplicates_count} possibly duplicate event."
   end
