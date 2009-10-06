67dd44b7dc3f799782aa7de03f969600f4e72691
James Darling <james@abscond.org>
2009-10-06 15:38:50 +0100

    forgotten files

---
+++ app/models/event_mailer.rb
@@ -0,0 +1,16 @@
+class EventMailer < ActionMailer::Base
+  def succesfully_added(event)
+    subject    "Your Learning Revolution event has been saved"
+    recipients event.contact_email_address
+    from       'Learning Revolution <noreply@learningrevolution.direct.gov.uk>'
+    body       :event => event
+  end
+
+  def succesfully_published(event)
+    subject    "Your Learning Revolution event has been approved and published"
+    recipients event.contact_email_address
+    from       'Learning Revolution <noreply@learningrevolution.direct.gov.uk>'
+    body       :event => event
+  end
+
+end
---
+++ spec/models/event_mailer_spec.rb
@@ -0,0 +1,41 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe EventMailer do
+  include EmailSpec::Helpers
+  include EmailSpec::Matchers
+  
+  describe "succesfully added email" do
+    before(:each) do
+      @event = mock_model(Event, :title => nil, :contact_email_address => "test@abscond.org", :start => Time.now.utc, :end => nil, :venue => nil, :slug => '1', :bitly_url => "http://bit.ly/skdjfg", :name => "Test event name")
+      @email = EventMailer.create_succesfully_added(@event)
+    end
+  
+    it "should be set to be delivered to the correct email address" do
+      @email.should deliver_to("test@abscond.org")
+    end
+    
+    it "should contain the event name" do
+      @email.body.should include("Test event name")
+    end
+  end
+  
+  describe "succesfully posted email" do
+    before(:each) do
+      @event = mock_model(Event, :title => nil, :contact_email_address => "test@abscond.org", :start => Time.now.utc, :end => nil, :venue => nil, :slug => '1', :bitly_url => "http://bit.ly/skdjfg", :name => "Test event name")
+      @email = EventMailer.create_succesfully_published(@event)
+    end
+  
+    it "should be set to be delivered to the correct email address" do
+      @email.should deliver_to("test@abscond.org")
+    end
+    
+    it "should contain the event name" do
+      @email.body.should include("Test event name")
+    end
+    
+    it "should include the bitly event URL" do
+      @email.body.should include("http://bit.ly/skdjfg")
+    end
+  end
+  
+end
