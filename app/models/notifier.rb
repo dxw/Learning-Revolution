b84519950572c6b03f7f953cb6d908becdc8e251
Tom Adams <tom@holizz.com>
2010-04-14 10:12:33 +0100

    Don't notify us on error.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ActionMailer::Base
 
   def error_notification(error_msg,exception,request)
-    recipients ['alerts@thedextrousweb.com']
+    recipients []
     from       "the-server@learning.dev.thedextrousweb.com"
     subject    "Learning Rev: #{error_msg} #{exception.andand.class}"
     body       :message => error_msg, :exception => exception, :referer => request.env['HTTP_REFERER'], :useragentstring => request.env['HTTP_USER_AGENT'], :remote_ip => request.env['REMOTE_ADDR'], :requesturi => request.env['REQUEST_URI'], :host => request.env['HTTP_HOST'], :request => request
